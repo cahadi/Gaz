fd932af18505b3de50fb4a7b09d019bcc590326c
cahadi <89908868+cahadi@users.noreply.github.com>
2023-03-16 03:02:51 +1000

    Add project files.

---
+++ Gaz/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace Gaz
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllersWithViews().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			app.UseRouting();
+			app.UseStaticFiles();
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					"default",
+					"{controller=Home}/{action=Index}/{id?}"
+					);
+			});
+		}
+	}
+}
